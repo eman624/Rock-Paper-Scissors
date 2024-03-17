cba90554b604d4d51a71dc3f42db97d0a5986c29
Edgar <edgar.noel.m@gmail.com>
2024-03-16 18:30:21 -0700

    implemented getComputerChoice() and base code from site

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-``
+function playRound(playerSelection, computerSelection) {
+  console.log(`player selection: ${playerSelection}`);
+  console.log(`computer selection: ${computerSelection}`);
+
+  if (playerSelection === computerSelection) {
+    return "tie";
+  } else if (computerSelection === "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (computerSelection === "scissors") {
+    return "You win! Rock Beats Scissors";
+  }
+}
+
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3);
+
+  if (randomNum === 0) {
+    return "rock";
+  } else if (randomNum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

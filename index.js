82f7356033d92d6d20423140756c9591c79a9a58
Edgar <edgar.noel.m@gmail.com>
2024-03-19 23:16:37 -0700

    refactored playRound(x,y)

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
+const Score = { playerScore: 0, computerScore: 0 };
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "You tie! Neither player wins!";
-  }
-
-  if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+  } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
     return "You Win! Rock beats Scissors";
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
     return "You Lose! Paper beats Rock";
-  }
-
-  if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
     return "You Win! Paper beats Rock";
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
     return "You Lose! Scissors beats Paper";
-  }
-
-  if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+  } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
     return "You Win! Scissors beats Paper";
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
     return "You Lose! Rock beats Scissors";
   }
 }
 
+let pressedButton = { data: "NONE" };
 function getPlayerChoice() {
-  const buttons = document.querySelectorAll("button");
-  const pressedButton = undefined;
+  const rockBtn = document.getElementById("ROCK");
+  const paperBtn = document.getElementById("PAPER");
+  const scissorsBtn = document.getElementById("SCISSORS");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      alert(button.id);
-      pressedButton = button.id;
-    });
+	rockBtn.addEventListener("click", => {
+
   });
-
-  console.log(pressedButton);
+  paperBtn.addEventListener("click", function (e) {});
+  scissorsBtn.addEventListener("click", function (e) {});
 }
 
-const gameOptions = { ROCK: "ROCK", PAPER: "PAPER", SCISSORS: "SCISSORS" };
-
 function getComputerChoice() {
-  var randNum = Math.floor(Math.random() * Object.keys(gameOptions).length);
-  var randOption = gameOptions[Object.keys(gameOptions)[randNum]];
+  const gameOptions = { ROCK: "ROCK", PAPER: "PAPER", SCISSORS: "SCISSORS" };
+  let randNum = Math.floor(Math.random() * Object.keys(gameOptions).length);
+  let randOption = gameOptions[Object.keys(gameOptions)[randNum]];
 
   return randOption;
 }
 
-const Score = { playerScore: 0, computerScore: 0 };
+
 
 function playGame() {
-  const playerSelection = getPlayerChoice();
-  //   const computerSelection = getComputerChoice();
+//   const playerSelection = getPlayerChoice();
+//   const computerSelection = getComputerChoice();
 
   //   const roundOutCome = playRound(playerSelection, computerSelection);
   console.log(`Player selection: ${playerSelection}`);
-  //   console.log(`Computer selection: ${computerSelection}`);
+  console.log(`Computer selection: ${computerSelection}`);
   //   checkScore(roundOutCome, Score);
   //   console.log(roundOutCome);
 
